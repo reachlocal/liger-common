e06d90aa2d27115995fa0748babc7f00206b73e6
John Gustafsson <john.gustafsson.liger@gmail.com>
2015-03-05 11:29:00 -0600

    Trying out requirejs

---
+++ app/vendor/cordovabridge.js
@@ -0,0 +1,79 @@
+define(['vendor/cordova.js'], function() {
+  // Container for all custom liger dingo plugin functions
+
+  var exec = cordova.require('cordova/exec');
+  return {
+    PAGE: null,
+
+    openPage: function(title, page, args, options) {
+      if (options === undefined) {
+        options = {};
+      }
+
+      exec(null, null, "Liger", "openPage", [title, page, args, options]);
+    },
+
+    closePage: function() {
+      exec(null, null, "Liger", "closePage", []);
+    },
+
+    closeToPage: function(page) {
+      exec(null, null, "Liger", "closePage", [page]);
+    },
+
+    updateParent: function(args) {
+      exec(null, null, "Liger", "updateParent", [null, args]);
+    },
+
+    updateParentPage: function(page, args) {
+      exec(null, null, "Liger", "updateParent", [page, args]);
+    },
+
+    childUpdates: function(args) {
+      PAGE.childUpdates(args);
+    },
+
+    openPageArguments: function(args) {
+      PAGE.args = args;
+    },
+
+    getPageArgs: function() {
+      var thePage = this.PAGE;
+      exec(
+        function(args) {
+          thePage.gotPageArgs(args);
+        },
+        function(error) {
+          return false;
+        }, "Liger", "getPageArgs", []);
+    },
+
+    openDialog: function(page, args, options) {
+      if (options === undefined) {
+        options = {};
+      }
+
+      exec(null, null, "Liger", "openDialog", [page, args, options]);
+    },
+
+    openDialogWithTitle: function(title, page, args, options) {
+      if (options === undefined) {
+        options = {};
+      }
+
+      exec(null, null, "Liger", "openDialogWithTitle", [title, page, args, options]);
+    },
+
+    closeDialog: function(args) {
+      exec(null, null, "Liger", "closeDialog", [args]);
+    },
+
+    closeDialogArguments: function(args) {
+      PAGE.closeDialogArguments(args);
+    },
+
+    toolbar: function(items) {
+      exec(null, null, "Liger", "toolbar", [items]);
+    }
+  };
+});
