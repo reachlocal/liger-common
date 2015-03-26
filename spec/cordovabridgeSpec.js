b4f4a3962ea14668eff83940cb538941f331c02d
John Gustafsson <john.gustafsson.liger@gmail.com>
2015-03-26 12:47:32 -0500

    Rewrote the callbacks.

--- spec/cordovabridgeSpec.js
+++ spec/cordovabridgeSpec.js
@@ -4,21 +4,12 @@ define(['cordovabridge'], function(bridge) {
       it('listens to device ready', function() {
         spyOn(document, 'addEventListener');
 
-        bridge.bindEvents();
+        bridge.bindEvents(function(){});
 
         expect(document.addEventListener).toHaveBeenCalledWith('deviceready', jasmine.anything(), jasmine.anything());
       });
     });
 
-    describe('#onDeviceReady', function() {
-      it('calls pageInit when the device is ready', function() {
-        spyOn(bridge.PAGE, 'pageInit');
-
-        bridge.onDeviceReady();
-
-        expect(bridge.PAGE.pageInit).toHaveBeenCalled();
-      });
-    });
   });
 
   describe('Bridge API', function() {
@@ -88,27 +79,28 @@ define(['cordovabridge'], function(bridge) {
     describe("#getPageArgs", function() {
       it("gets called", function() {
         spyOn(bridge, 'exec');
-        bridge.getPageArgs();
+        bridge.getPageArgs(function(args){});
 
         expect(bridge.exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Liger", "getPageArgs", []);
       });
 
       it("gets called and executes successfully", function() {
-        spyOn(bridge.PAGE, 'gotPageArgs');
+        var test = {gotPageArgs:function(args){}};
+        spyOn(test, 'gotPageArgs');
         spyOn(bridge, 'exec').and.callFake(function(successCallback, failCallback, service, action, actionArgs) {
           successCallback();
         });
-        bridge.getPageArgs();
+        bridge.getPageArgs(test.gotPageArgs);
 
         expect(bridge.exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Liger", "getPageArgs", []);
-        expect(bridge.PAGE.gotPageArgs).toHaveBeenCalled();
+        expect(test.gotPageArgs).toHaveBeenCalled();
       });
 
       it("gets called and fails", function() {
         spyOn(bridge, 'exec').and.callFake(function(successCallback, failCallback, service, action, actionArgs) {
           failCallback();
         });
-        bridge.getPageArgs();
+        bridge.getPageArgs(function(args){});
 
         expect(bridge.exec).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function), "Liger", "getPageArgs", []);
       });
@@ -182,12 +174,12 @@ define(['cordovabridge'], function(bridge) {
     });
 
     it("#closeDialogArguments", function() {
-      spyOn(bridge.PAGE, 'closeDialogArguments');
+      spyOn(PAGE, 'closeDialogArguments');
       bridge.closeDialogArguments({
         'foo': 'bar'
       });
 
-      expect(bridge.PAGE.closeDialogArguments).toHaveBeenCalledWith({
+      expect(PAGE.closeDialogArguments).toHaveBeenCalledWith({
         'foo': 'bar'
       });
     });
@@ -201,5 +193,96 @@ define(['cordovabridge'], function(bridge) {
       ]);
     });
 
-  });
+  }); // Bridge API
+  describe('callbacks', function() {
+      describe('#addChildUpdates', function() {
+      it('can be added', function() {
+        var update = {test:'data'};
+        var test = { childUpdates:function(update) {}};
+        spyOn(test, 'childUpdates');
+        bridge.addChildUpdates(test.childUpdates);
+        PAGE.childUpdates(update);
+        expect(test.childUpdates).toHaveBeenCalledWith(update);
+      });
+    });
+
+    describe('#addCloseDialogArguments', function() {
+      it('can be added', function() {
+        var args = {test:'data'};
+        var test = { closeDialogArguments:function(args) {}};
+        spyOn(test, 'closeDialogArguments');
+        bridge.addCloseDialogArguments(test.closeDialogArguments);
+        PAGE.closeDialogArguments(args);
+        expect(test.closeDialogArguments).toHaveBeenCalledWith(args);
+      });
+    });
+
+    describe('#addHandleAppOpenURL', function() {
+      it('can be added', function() {
+        var url = 'ligermobile://isGreat/yes';
+        var test = { handleAppOpenURL:function(args) {}};
+        spyOn(test, 'handleAppOpenURL');
+        bridge.addHandleAppOpenURL(test.handleAppOpenURL);
+        PAGE.handleAppOpenURL(url);
+        expect(test.handleAppOpenURL).toHaveBeenCalledWith(url);
+      });
+    });
+
+    describe('#addHeaderButtonTapped', function() {
+      it('can be added', function() {
+        var button = 'done';
+        var test = { headerButtonTapped:function(button) {}};
+        spyOn(test, 'headerButtonTapped');
+        bridge.addHeaderButtonTapped(test.headerButtonTapped);
+        PAGE.headerButtonTapped(button);
+        expect(test.headerButtonTapped).toHaveBeenCalledWith(button);
+      });
+    });
+
+    describe('#addNotificationArrived', function() {
+      it('can be added', function() {
+        var payload = {notification:'yes please'};
+        var test = { notificationArrived:function(payload, background) {}};
+        spyOn(test, 'notificationArrived');
+        bridge.addNotificationArrived(test.notificationArrived);
+        PAGE.notificationArrived(payload, false);
+        expect(test.notificationArrived).toHaveBeenCalledWith(payload, false);
+      });
+    });
+
+    describe('#addOnPageAppear', function() {
+      it('can be added', function() {
+        var test = { pageAppear:function() {}};
+        spyOn(test, 'pageAppear');
+        bridge.addOnPageAppear(test.pageAppear);
+        PAGE.onPageAppear();
+        expect(test.pageAppear).toHaveBeenCalled();
+      });
+    });
+
+    describe('#addCallback', function() {
+      it('adds a function', function() {
+        var test = { test:function(){} };
+        spyOn(test, 'test');
+        PAGE._addCallback('test', test.test);
+        PAGE._callCallback('test');
+        expect(test.test).toHaveBeenCalled();
+      });
+    });
+
+    describe('#addPushNotificationTokenUpdated', function() {
+      it('can be added', function() {
+        var token = '1234';
+        var type = 'test';
+        var error = '';
+        var test = { pushNotificationTokenUpdated:function() {}};
+        spyOn(test, 'pushNotificationTokenUpdated');
+        bridge.addPushNotificationTokenUpdated(test.pushNotificationTokenUpdated);
+        PAGE.pushNotificationTokenUpdated(token, type, error);
+        expect(test.pushNotificationTokenUpdated).toHaveBeenCalledWith(token, type, error);
+      });
+    });
+
+  }); // callbacks
+
 });
--- spec/ligermobileSpec.js
+++ spec/ligermobileSpec.js
@@ -110,177 +110,260 @@ define(['ligermobile', 'cordovabridge'], function() {
       });
     });
 
-    describe("LIGER api", function() {
+  });
 
+  describe('page API', function() {
+    describe("#addToolbar", function() {
+      it("should do nothing if toolbarItems is null", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'toolbar');
+
+        lm.addToolbar();
+
+        expect(bridge.toolbar).not.toHaveBeenCalled();
+      });
+
+      it("should initialize the toolbar if toolbarItems are set", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'toolbar');
+        lm.toolbarItems = [{}, {}];
+        lm.addToolbar();
+
+        expect(bridge.toolbar).toHaveBeenCalledWith(lm.toolbarItems);
+      });
     });
 
-    describe('page API', function() {
-      describe("#addToolbar", function() {
-        it("should do nothing if toolbarItems is null", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'toolbar');
+    describe("#openPage", function() {
+      it("opens a new page if the current page is a collection page", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'openPage');
 
-          lm.addToolbar();
-
-          expect(bridge.toolbar).not.toHaveBeenCalled();
+        lm.openPage('test', 'test', {
+          'foo': 'bar'
+        }, {
+          'bar': 'foo'
         });
 
-        it("should initialize the toolbar if toolbarItems are set", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'toolbar');
-          lm.toolbarItems = [{}, {}];
-          lm.addToolbar();
-
-          expect(bridge.toolbar).toHaveBeenCalledWith(lm.toolbarItems);
+        expect(bridge.openPage).toHaveBeenCalledWith('test', 'test', {
+          'foo': 'bar'
+        }, {
+          'bar': 'foo'
         });
       });
+    });
 
-      describe("#openPage", function() {
-        it("opens a new page if the current page is a collection page", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'openPage');
+    describe("#closePage", function() {
+      it("closes the current page (if in a collection and not the last page)", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'closePage');
 
-          lm.openPage('test', 'test', {
-            'foo': 'bar'
-          }, {
-            'bar': 'foo'
-          });
+        lm.closePage();
 
-          expect(bridge.openPage).toHaveBeenCalledWith('test', 'test', {
-            'foo': 'bar'
-          }, {
-            'bar': 'foo'
-          });
+        expect(bridge.closePage).toHaveBeenCalled();
+      });
+    });
+
+    describe("#closeToPage", function() {
+      it("closes the current page and all previous pages until the named page is found (if in a collection and not the last page)", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'closeToPage');
+        lm.closeToPage('test');
+
+        expect(bridge.closeToPage).toHaveBeenCalledWith('test');
+      });
+    });
+
+    describe("#updateParent", function() {
+      it("update the parent's page with a json structure", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'updateParent');
+
+        lm.updateParent({
+          'foo': 'bar'
+        });
+
+        expect(bridge.updateParent).toHaveBeenCalledWith({
+          'foo': 'bar'
         });
       });
+    });
 
-      describe("#closePage", function() {
-        it("closes the current page (if in a collection and not the last page)", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'closePage');
+    describe("#updateParentPage", function() {
+      it("update the first parent page named with a json structure", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'updateParentPage');
+        lm.updateParentPage('hello', {
+          'foo': 'bar'
+        });
 
-          lm.closePage();
-
-          expect(bridge.closePage).toHaveBeenCalled();
+        expect(bridge.updateParentPage).toHaveBeenCalledWith('hello', {
+          'foo': 'bar'
         });
       });
+    });
 
-      describe("#closeToPage", function() {
-        it("closes the current page and all previous pages until the named page is found (if in a collection and not the last page)", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'closeToPage');
-          lm.closeToPage('test');
+    describe("#openDialog", function() {
+      it("opens up a dialog", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'openDialog');
 
-          expect(bridge.closeToPage).toHaveBeenCalledWith('test');
+        lm.openDialog('hello', {
+          'foo': 'bar'
+        }, {
+          'bar': 'foo'
+        });
+
+        expect(bridge.openDialog).toHaveBeenCalledWith('hello', {
+          'foo': 'bar'
+        }, {
+          'bar': 'foo'
         });
       });
+    });
 
-      describe("#updateParent", function() {
-        it("update the parent's page with a json structure", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'updateParent');
+    describe("#openDialogWithTitle", function() {
+      it("opens up a dialog with the page inside of a navigator with title", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'openDialogWithTitle');
 
-          lm.updateParent({
-            'foo': 'bar'
-          });
+        lm.openDialogWithTitle('hello', 'hello', {
+          'foo': 'bar'
+        }, {
+          'bar': 'foo'
+        });
 
-          expect(bridge.updateParent).toHaveBeenCalledWith({
-            'foo': 'bar'
-          });
+        expect(bridge.openDialogWithTitle).toHaveBeenCalledWith('hello', 'hello', {
+          'foo': 'bar'
+        }, {
+          'bar': 'foo'
         });
       });
+    });
 
-      describe("#updateParentPage", function() {
-        it("update the first parent page named with a json structure", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'updateParentPage');
-          lm.updateParentPage('hello', {
-            'foo': 'bar'
-          });
+    describe("#closeDialog", function() {
+      it("closes the currently open dialog if the page is in a dialog", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
 
-          expect(bridge.updateParentPage).toHaveBeenCalledWith('hello', {
-            'foo': 'bar'
-          });
+        spyOn(bridge, 'closeDialog');
+        lm.closeDialog({
+          'foo': 'bar'
+        });
+
+        expect(bridge.closeDialog).toHaveBeenCalledWith({
+          'foo': 'bar'
         });
       });
+    });
 
-      describe("#openDialog", function() {
-        it("opens up a dialog", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'openDialog');
+    describe("#toolbar", function() {
+      it("sets the toolbar items on the toolbar", function() {
+        var lm = require('ligermobile');
+        var bridge = require('cordovabridge');
+        spyOn(bridge, 'toolbar');
 
-          lm.openDialog('hello', {
-            'foo': 'bar'
-          }, {
-            'bar': 'foo'
-          });
+        lm.toolbar(['test', 'test1', 'test2']);
 
-          expect(bridge.openDialog).toHaveBeenCalledWith('hello', {
-            'foo': 'bar'
-          }, {
-            'bar': 'foo'
-          });
-        });
+        expect(bridge.toolbar).toHaveBeenCalledWith(['test', 'test1', 'test2']);
       });
+    });
+  }); // 'page API'
 
-      describe("#openDialogWithTitle", function() {
-        it("opens up a dialog with the page inside of a navigator with title", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'openDialogWithTitle');
-
-          lm.openDialogWithTitle('hello', 'hello', {
-            'foo': 'bar'
-          }, {
-            'bar': 'foo'
-          });
-
-          expect(bridge.openDialogWithTitle).toHaveBeenCalledWith('hello', 'hello', {
-            'foo': 'bar'
-          }, {
-            'bar': 'foo'
-          });
-        });
+  describe('callbacks', function() {
+    describe('#addChildUpdates', function() {
+      it('can be added', function() {
+        var update = {test:'data'};
+        var lm = require('ligermobile');
+        var test = { childUpdates:function(update) {}};
+        spyOn(test, 'childUpdates');
+        lm.addChildUpdates(test.childUpdates);
+        PAGE.childUpdates(update);
+        expect(test.childUpdates).toHaveBeenCalledWith(update);
       });
+    });
 
-      describe("#closeDialog", function() {
-        it("closes the currently open dialog if the page is in a dialog", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-
-          spyOn(bridge, 'closeDialog');
-          lm.closeDialog({
-            'foo': 'bar'
-          });
-
-          expect(bridge.closeDialog).toHaveBeenCalledWith({
-            'foo': 'bar'
-          });
-        });
+    describe('#addCloseDialogArguments', function() {
+      it('can be added', function() {
+        var args = {test:'data'};
+        var lm = require('ligermobile');
+        var test = { closeDialogArguments:function(args) {}};
+        spyOn(test, 'closeDialogArguments');
+        lm.addCloseDialogArguments(test.closeDialogArguments);
+        PAGE.closeDialogArguments(args);
+        expect(test.closeDialogArguments).toHaveBeenCalledWith(args);
       });
+    });
 
-      describe("#toolbar", function() {
-        it("sets the toolbar items on the toolbar", function() {
-          var lm = require('ligermobile');
-          var bridge = require('cordovabridge');
-          spyOn(bridge, 'toolbar');
-
-          lm.toolbar(['test', 'test1', 'test2']);
-
-          expect(bridge.toolbar).toHaveBeenCalledWith(['test', 'test1', 'test2']);
-        });
+    describe('#addHandleAppOpenURL', function() {
+      it('can be added', function() {
+        var url = 'ligermobile://isGreat/yes';
+        var lm = require('ligermobile');
+        var test = { handleAppOpenURL:function(url) {}};
+        spyOn(test, 'handleAppOpenURL');
+        lm.addHandleAppOpenURL(test.handleAppOpenURL);
+        PAGE.handleAppOpenURL(url);
+        expect(test.handleAppOpenURL).toHaveBeenCalledWith(url);
       });
+    });
 
+    describe('#addHeaderButtonTapped', function() {
+      it('can be added', function() {
+        var button = 'done';
+        var lm = require('ligermobile');
+        var test = { headerButtonTapped:function(button) {}};
+        spyOn(test, 'headerButtonTapped');
+        lm.addHeaderButtonTapped(test.headerButtonTapped);
+        PAGE.headerButtonTapped(button);
+        expect(test.headerButtonTapped).toHaveBeenCalledWith(button);
+      });
+    });
 
-    }); // 'page API'
+    describe('#addNotificationArrived', function() {
+      it('can be added', function() {
+        var payload = {notification:'yes please'};
+        var lm = require('ligermobile');
+        var test = { notificationArrived:function(payload, background) {}};
+        spyOn(test, 'notificationArrived');
+        lm.addNotificationArrived(test.notificationArrived);
+        PAGE.notificationArrived(payload, false);
+        expect(test.notificationArrived).toHaveBeenCalledWith(payload, false);
+      });
+    });
 
-  });
+    describe('#addOnPageAppear', function() {
+      it('can be added', function() {
+        var lm = require('ligermobile');
+        var test = { pageAppear:function() {}};
+        spyOn(test, 'pageAppear');
+        lm.addOnPageAppear(test.pageAppear);
+        PAGE.onPageAppear();
+        expect(test.pageAppear).toHaveBeenCalled();
+      });
+    });
+
+    describe('#addPushNotificationTokenUpdated', function() {
+      it('can be added', function() {
+        var token = '1234';
+        var type = 'test';
+        var error = '';
+        var lm = require('ligermobile');
+        var test = { pushNotificationTokenUpdated:function() {}};
+        spyOn(test, 'pushNotificationTokenUpdated');
+        lm.addPushNotificationTokenUpdated(test.pushNotificationTokenUpdated);
+        PAGE.pushNotificationTokenUpdated(token, type, error);
+        expect(test.pushNotificationTokenUpdated).toHaveBeenCalledWith(token, type, error);
+      });
+    });
+
+  }); // callbacks
+
 });
